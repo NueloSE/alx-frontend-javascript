388afcaef5cfcfbab25331cb5946211db0474eef
Nuelo <akalo.emmanuel18@gmail.com>
2024-10-09 11:00:01 +0100

    feat: update code implementation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,12 +2,11 @@ export default function getListStudentIds(studentArray) {
   if (!(studentArray instanceof Array)) {
     return [];
   }
-
-  const studentMap = new Map();
   const studentKeys = [];
-  for (const student of studentArray) {
-    studentMap.set(student.id, student);
-    studentKeys.push(student.id);
-  }
+
+  studentArray.map(student => {
+	studentKeys.push(student.id);
+  })
+
   return (studentKeys);
 }
