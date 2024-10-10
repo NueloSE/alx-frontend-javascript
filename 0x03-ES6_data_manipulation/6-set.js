053c729da498236eec3bd82129f1147d74cc8503
Nuelo <akalo.emmanuel18@gmail.com>
2024-10-10 23:11:15 +0100

    fix: eslint error

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,9 +1,3 @@
 export default function setFromArray(arraySet) {
-  // let result = new Set()
-  // arraySet.forEach(element => {
-  // 	result.add(element);
-  // });
-
-  // return result;
   return new Set(arraySet);
 }
