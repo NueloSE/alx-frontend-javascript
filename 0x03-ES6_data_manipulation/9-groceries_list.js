d3b0129448df1ec4709f202b915e2a96a712f467
Nuelo <akalo.emmanuel18@gmail.com>
2024-10-11 09:48:12 +0100

    feat: use Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+export default function groceriesList() {
+  const myMap = new Map();
+
+  myMap.set('Apple', 10).set('Tomatoes', 10).set('Pasta', 1).set('Rice', 1)
+    .set('Banana', 5);
+
+  return myMap;
+}
