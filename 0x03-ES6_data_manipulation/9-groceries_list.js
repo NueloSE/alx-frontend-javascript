74986f96b4ac9effd28956290b02544f1ba556bc
Nuelo <akalo.emmanuel18@gmail.com>
2024-10-11 15:21:38 +0100

    fix: map content

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,7 @@
 export default function groceriesList() {
   const myMap = new Map();
 
-  myMap.set('Apple', 10).set('Tomatoes', 10).set('Pasta', 1).set('Rice', 1)
+  myMap.set('Apples', 10).set('Tomatoes', 10).set('Pasta', 1).set('Rice', 1)
     .set('Banana', 5);
 
   return myMap;
